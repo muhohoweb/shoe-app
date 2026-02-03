010663c320edcf02f229686f75f43530412baec6
muhohoweb <muhohoweb@icloud.com>
2026-02-03 20:23:48 +0300

    Add transactions page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\CategoryController;
 
 Route::get('/', [ShopController::class, 'index'])->name('home');
 
+
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
