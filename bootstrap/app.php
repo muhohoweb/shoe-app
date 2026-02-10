d36a1785ec4f25af9ff74204fe0bcd98c597c490
muhohoweb <muhohoweb@icloud.com>
2026-02-10 09:37:23 +0300

    Mpesa default shortcode

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'mpesa/callback',
             'mpesa/balance/callback',
+            'mpesa/status/result',
             'whatsapp/webhook',
             'whatsapp/webhook/',
             'whatsapp/send-dispatch/',
